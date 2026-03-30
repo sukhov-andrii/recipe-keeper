0322b0bc409bb49e5e97b9fded86eebadfd51476
Andrii Sukhov <sukhov.andrii@gmail.com>
2026-03-30 18:14:51 +0200

    Initial Spring Boot setup

---
+++ src/main/java/nl/inholland/recipekeeper/RecipekeeperApplication.java
@@ -0,0 +1,13 @@
+package nl.inholland.recipekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipekeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipekeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/inholland/recipekeeper/RecipekeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.inholland.recipekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipekeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
