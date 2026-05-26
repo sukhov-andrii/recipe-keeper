8b8186cddd246246a1a7437b23d68db443a5469d
Andrii Sukhov <sukhov.andrii@gmail.com>
2026-05-26 11:54:06 +0200

    feat: add JPA repositories for recipe domain

---
+++ src/main/java/nl/inholland/recipekeeper/repository/RecipeRepository.java
@@ -0,0 +1,26 @@
+package nl.inholland.recipekeeper.repository;
+
+import nl.inholland.recipekeeper.model.entity.entity.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RecipeRepository extends JpaRepository<Recipe, UUID> {
+
+    Optional<Recipe> findBySourceMealId(String sourceMealId);
+
+    // TODO: verify if that is correct
+    @Query("""
+    SELECT DISTINCT r FROM Recipe r
+    LEFT JOIN r.recipeIngredients ri
+    LEFT JOIN ri.ingredient i
+    WHERE LOWER(r.title) LIKE LOWER(CONCAT('%', :query, '%'))
+       OR LOWER(i.name) LIKE LOWER(CONCAT('%', :query, '%'))
+""")
+    List<Recipe> searchCandidates(@Param("query") String query);
+}
+
